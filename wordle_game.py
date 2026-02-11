9915ef8bc2efe6582e021291122cd4b861c86403
Kaliny4 <hanna.kalinichenko@gmail.com>
2026-02-11 15:02:25 +0100

    second commit

--- wordle_game.py
+++ wordle_game.py
@@ -1,25 +1,46 @@
-from colorama import Fore, Back, Style, init 
+from colorama import Fore, Back, Style, init
 init()
 import random
+import streamlit as st
+
+st.title("Wordle Game")
+st.text("""The New York Times game, Wordle.
+        The game selects a secret word from a list of possible words. The player must then guess this word. 
+        After each guess, the game should provide feedback on correct letters and correct positions. 
+        The game should end when the user has guessed the correct word or has used all 6 attempts. 
+        There are 3 types of the feedback: 
+• A letter in the word is green if your guess for that letter in that position is correct.
+• A letter is yellow if that letter exists in the word but not in that position.
+• A letter is gray if that letter does not exist in the word.
+        """)
+
+with open("wordle_ord.txt", "r") as f:
+    wordlist = [line.strip() for line in f]
+
 
 def correct_place(letter):
     return Back.GREEN + letter + Back.RESET
 
+
 def correct_letter(letter):
     return Back.YELLOW + letter + Back.RESET
 
+
 def incorrect_letter(letter):
     return Style.DIM + letter + Back.RESET
+
 
 def compare(input_from_user, word):
     output = [""] * len(input_from_user)
     remaining_letters = list(word)
 
+    # Mark correct letter and position - Green
     for i, letter in enumerate(input_from_user):
         if letter == remaining_letters[i]:
             output[i] = correct_place(letter)
-            remaining_letters[i] = None 
+            remaining_letters[i] = None
 
+    # Mark correct letter anbut not position - Yellow
     for i, letter in enumerate(input_from_user):
         if output[i]:
             continue
@@ -28,46 +49,48 @@ def compare(input_from_user, word):
             remaining_letters[remaining_letters.index(letter)] = None
         else:
             output[i] = incorrect_letter(letter)
-            
-    return ''.join(output)
 
-with open('wordle_ord.txt', 'r') as f:
-    wordlist = [line.strip() for line in f]
+    return "".join(output)
 
 
-while True:
-    print(Back.WHITE + Fore.BLACK + "Start the game? (yes/no))" + Style.RESET_ALL)
-    input_from_user = input()
-    if input_from_user == "no":
-        break
-    elif input_from_user == "yes":
-        attempts = 0
-        word = random.choice(wordlist)
-        print("Enter your word: ")
-
-        while attempts < 6:
-            temp_user_word = input().lower()
-            if temp_user_word == "no":
-                break
-            if len(temp_user_word) != 5 or not temp_user_word.isalpha() or temp_user_word not in wordlist:
-                print("Please enter a 5 letter valid word")
-                continue
-            
-            user_word = temp_user_word
-
-            result = compare(user_word, word)
-            print(result)
-            if word == user_word:
-                print("Congrats")
-                attempts = 6
-                break
+def check_word(wordlist):
+    word = random.choice(wordlist)
+    attempts = 0
+    max_attempts = 6
+    while attempts < max_attempts:
+        user_word = input().lower()
+        if user_word == "no":
+            return False
+        if len(user_word) != 5 or not user_word.isalpha() or user_word not in wordlist:
+            print("Please enter a 5 letter valid word")
+            continue
+        elif user_word == word:
+            print(Back.WHITE + Fore.RED + "Congrats! the word was:" + word)
+            return True
+        else:
+            print(compare(user_word, word))
             attempts += 1
-            
-        print("The word was: ", word)
+            print("Try again. Attempts left: ", max_attempts - attempts)
 
-    else:
-        print("Please enter yes or no")
+    print("You reached max attempts. The word was:", word)
+    exit()
 
-   
-print("Thanks for playing!")
-    
+
+def main():
+    while True:
+        print(Back.WHITE + Fore.BLACK + "Start the game? (yes/no))" + Style.RESET_ALL)
+        input_from_user = input()
+        if input_from_user == "no":
+            break
+        elif input_from_user == "yes":
+            print("Enter your word: ")
+            check_word(wordlist)
+
+        else:
+            print("Please enter yes or no")
+
+    print("Thanks for playing!")
+
+
+if __name__ == "__main__":
+    main()
